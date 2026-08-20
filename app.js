0673a5df1b64d5ca65d73edd71ea9a6f5c60f962
putraswasta <putra.swasta.jaya@gmail.com>
2026-08-21 00:28:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const PACK_TIMEOUT = 10000; // 10 detik
 
-const packs = {};
-
-function updatePack(packId, data) {
-    packs[packId] = {
-        ...data,
-        lastSeen: Date.now()
-    };
-
-    renderPacks();
-}
-
-function removeOfflinePacks() {
+function removeStalePacks() {
     const now = Date.now();
-    let changed = false;
 
     Object.keys(packs).forEach(packId => {
-        if (now - packs[packId].lastSeen > PACK_TIMEOUT) {
+        if (
+            !packs[packId].lastSeen ||
+            now - packs[packId].lastSeen > PACK_TIMEOUT
+        ) {
             delete packs[packId];
-            changed = true;
-            console.log(`[OFFLINE] ${packId} dihapus dari dashboard`);
         }
     });
 
-    if (changed) {
-        renderPacks();
-    }
+    render();
 }
 
-setInterval(removeOfflinePacks, 1000);
+setInterval(removeStalePacks, 1000);
